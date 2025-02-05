3c269c3cead79ffa284b8ce994e7778ae8c16a4d
sunnaylya <79587455+sunnaylya@users.noreply.github.com>
2025-02-05 17:03:20 +0500

    Add files via upload

---
+++ car/car.js
@@ -0,0 +1,20 @@
+const carName = "Ford Focus";
+alert(carName);
+let maxCarSpeed = "210 км/ч";
+alert(maxCarSpeed);
+let carOwner = " ";
+alert(carOwner);
+
+maxCarSpeed = "190 км/ч";
+console.log('У машины изменилась скорость:', maxCarSpeed);
+maxCarSpeed = "180 км/ч";
+console.log('У машины изменилась скорость:', maxCarSpeed);
+maxCarSpeed = "170 км/ч";
+console.log('У машины изменилась скорость:', maxCarSpeed);
+
+carOwner = "Антон";
+console.log("У машины изменился владелец:", carOwner);
+carOwner = "Милана";
+console.log("У машины изменился владелец:", carOwner);
+carOwner = "Сэм";
+console.log("У машины изменился владелец:", carOwner);
